132827fb285a1442ff0074eaf68ea0ccb30bf448
alinashch <83130878+alinashch@users.noreply.github.com>
2023-10-30 19:59:46 +0300

    acsac

---
+++ ApiGateway_module/src/test/java/com/example/apigateway_module/ApiGatewayModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigateway_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
